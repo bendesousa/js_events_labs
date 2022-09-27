66ff43178fbfe6668ffe97b2d73bb40a333c09b7
bendesousa <bdsousa069@gmail.com>
2022-09-27 13:47:14 +0100

    can add item to list

--- lab_jsevents_start_code/script.js
+++ lab_jsevents_start_code/script.js
@@ -1,8 +1,13 @@
 const enterBtn = document.getElementById("enter");
-const list = document.querySelector(".list")
+const list = document.querySelector("#list")
 
 enterBtn.addEventListener("click", ()=> {
     const listItem1 = document.createElement("li");
     listItem1.innerText = document.getElementById("new-todo");
     list.appendChild(listItem1);
 });
+
+// const listItem1 = document.createElement("li");
+// listItem1.innerText = "Hello";
+// // listItem1.innerText = document.getElementById("new-todo");
+// list.appendChild(listItem1);
