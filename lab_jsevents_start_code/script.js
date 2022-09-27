65fef78146bddcb567d104e509a29784510e7322
bendesousa <bdsousa069@gmail.com>
2022-09-27 15:33:06 +0100

    added a checkbox

--- lab_jsevents_start_code/script.js
+++ lab_jsevents_start_code/script.js
@@ -5,9 +5,14 @@ function createItem(){
     const listItem1 = document.createElement("li");
     listItem1.innerText = document.getElementById("new-todo").value;
     list.appendChild(listItem1);
+    
     const deleteBtn = document.createElement("button");
     deleteBtn.innerText ="Delete";
     listItem1.appendChild(deleteBtn).addEventListener("click", ()=> {list.removeChild(listItem1)});
+    
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    listItem1.appendChild(checkBox).addEventListener("click", completed);
 }
 
 
@@ -17,6 +22,8 @@ const dateBtn = document.getElementById("date");
 dateBtn.addEventListener("click", ()=>{
     dateBtn.innerText = Date();
 })
+
+function completed ()
 
 
 
