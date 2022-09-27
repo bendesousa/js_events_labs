cad45f694f68e4787bb28435c86c31f24baa2553
bendesousa <bdsousa069@gmail.com>
2022-09-27 13:42:35 +0100

    added event listener to enter button

--- lab_jsevents_start_code/script.js
+++ lab_jsevents_start_code/script.js
@@ -1,3 +1,8 @@
 const enterBtn = document.getElementById("enter");
+const list = document.querySelector(".list")
 
-enterBtn.addEventListener("click", ()=> )
+enterBtn.addEventListener("click", ()=> {
+    const listItem1 = document.createElement("li");
+    listItem1.innerText = document.getElementById("new-todo");
+    list.appendChild(listItem1);
+});
