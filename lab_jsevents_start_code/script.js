264a8257b7cd4fb89cb42312846c7f021871291a
Kat Fagg <katjfagg@gmail.com>
2022-09-27 14:51:20 +0100

    refactored

--- lab_jsevents_start_code/script.js
+++ lab_jsevents_start_code/script.js
@@ -1,14 +1,22 @@
 const enterBtn = document.getElementById("enter");
 const list = document.querySelector("#list")
 
-enterBtn.addEventListener("click", ()=> {
+function createItem(){
     const listItem1 = document.createElement("li");
     listItem1.innerText = document.getElementById("new-todo").value;
     list.appendChild(listItem1);
     const deleteBtn = document.createElement("button");
     deleteBtn.innerText ="Delete";
     listItem1.appendChild(deleteBtn).addEventListener("click", ()=> {list.removeChild(listItem1)});
-});
+}
+
+
+enterBtn.addEventListener("click", createItem);
+
+const dateBtn = document.getElementById("date");
+dateBtn.addEventListener("click", ()=>{
+    dateBtn.innerText = Date();
+})
 
 
 
