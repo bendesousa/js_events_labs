a579193a9a9ec675fd8b496ff0d98afd8ea58e39
Kat Fagg <katjfagg@gmail.com>
2022-09-27 14:36:07 +0100

    Delete button function added

--- lab_jsevents_start_code/script.js
+++ lab_jsevents_start_code/script.js
@@ -5,7 +5,11 @@ enterBtn.addEventListener("click", ()=> {
     const listItem1 = document.createElement("li");
     listItem1.innerText = document.getElementById("new-todo").value;
     list.appendChild(listItem1);
-    // const deleteBtn = document.createElement("button");
-
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText ="Delete";
+    listItem1.appendChild(deleteBtn).addEventListener("click", ()=> {list.removeChild(listItem1)});
 });
+
+
+
 
